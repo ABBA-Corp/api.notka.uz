d1807ab27e0ec6c1ae934d13972af343730a9dfa
Kairat Sultan <garri.sultonov@mail.ru>
2023-01-24 17:57:57 +0600

    change swagger name

--- config/settings/base.py
+++ config/settings/base.py
@@ -304,8 +304,8 @@ CORS_URLS_REGEX = r"^/api/.*$"
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Orzu Candy API",
-    "DESCRIPTION": "Documentation of API endpoints of Orzu Candy",
+    "TITLE": "Notka.uz Candy API",
+    "DESCRIPTION": "Documentation of API endpoints of Notka.uz Candy",
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
 }
